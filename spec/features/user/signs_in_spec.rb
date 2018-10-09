67c853918f4d3c9ef75240dafa72d3d38e55b8f5
Xavi Ablaza <xlablaza@gmail.com>
2018-10-08 22:28:57 -0700

    Add user login tests

---
+++ spec/features/user/signs_in_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "User signs in", type: :feature do
+  let!(:user) { create(:user) }
+
+  context "with valid account" do
+    it "shows you the dashboard" do
+      visit new_user_session_path
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Log in"
+
+      expect(page).to have_content "Dashboard"
+    end
+  end
+
+  context "with invalid account" do
+    it "shows you an error message" do
+      visit new_user_session_path
+
+      fill_in "Email", with: "invalidemail@invalid.com"
+      fill_in "Password", with: "password"
+      click_on "Log in"
+
+      expect(page).to have_content "Invalid Email or password."
+    end
+  end
+
+end
