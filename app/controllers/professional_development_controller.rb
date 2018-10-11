5704f58b9cc0dd6700d32b855021b902df2074eb
Christopher Ruiz <ruiz165@mail.chapman.edu>
2018-10-10 22:10:01 -0700

    Populate navbar

---
+++ app/controllers/professional_development_controller.rb
@@ -0,0 +1,10 @@
+class ProfessionalDevelopmentController < ApplicationController
+  def exams
+  end
+
+  def books
+  end
+
+  def future
+  end
+end
---
+++ app/controllers/standards_resources_controller.rb
@@ -0,0 +1,16 @@
+class StandardsResourcesController < ApplicationController
+  def conduct
+  end
+
+  def standards
+  end
+
+  def scoring
+  end
+
+  def demographics
+  end
+
+  def podcasts
+  end
+end
