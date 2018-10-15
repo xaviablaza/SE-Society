011b67e1fd271a93d96c1ed3981b49f159b96f33
Matt McCortney <hazmatt101@gmail.com>
2018-10-15 16:29:05 -0700

    Initial user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   get 'standards_resources/conduct'
   get 'standards_resources/standards'
   get 'standards_resources/scoring'
@@ -9,13 +10,16 @@ Rails.application.routes.draw do
   get 'professional_development/future'
   
   devise_for :users
+=======
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+>>>>>>> User-signup stuff
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_scope :user do
-    authenticated do
-      root "dashboard#show"
-    end
+    devise_scope :user do
+      authenticated do
+        root "dashboard#show"
+      end
 
     unauthenticated do
       root "homepage#show"
