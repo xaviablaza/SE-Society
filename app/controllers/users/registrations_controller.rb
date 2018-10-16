7069e59967e4b5f4479e6a3997cd560e6182fdd8
Matt McCortney <hazmatt101@gmail.com>
2018-10-15 17:11:10 -0700

    Add additional fields to sign-up page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  private
+  protected
 
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
@@ -13,10 +13,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+   #GET /resource/sign_up
+   #def new
+   #  super
+   #end
 
   # POST /resource
   # def create
