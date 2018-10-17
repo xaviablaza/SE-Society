453f6d742b1890db21fbe4ce7a17f6e68829d69b
Xavi Ablaza <xlablaza@gmail.com>
2018-10-16 23:57:37 -0700

    Include tests for unique username

--- spec/features/user/register_spec.rb
+++ spec/features/user/register_spec.rb
@@ -1,15 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "User registers", type: :feature do
-  let!(:user) { build(:user) }
+  let!(:new_user) { build(:user) }
+  let!(:taken_user) { create(:user) }
 
   context "with non-matching passwords" do
     it "shows you an error message" do
       visit new_user_registration_path
 
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password
+      fill_in "Email", with: new_user.email
+      fill_in "Password", with: new_user.password
       fill_in "Password confirmation", with: "somethingelse"
+      fill_in "First name", with: new_user.first_name
+      fill_in "Last name", with: new_user.last_name
+      fill_in "Username", with: new_user.username
       click_on "Sign up"
 
       expect(page).to have_content "Password confirmation doesn't match Password"
@@ -20,12 +24,47 @@ RSpec.describe "User registers", type: :feature do
     it "shows you an error message" do
       visit new_user_registration_path
 
-      fill_in "Email", with: user.email
+      fill_in "Email", with: new_user.email
       fill_in "Password", with: "asdf"
       fill_in "Password confirmation", with: "asdf"
+      fill_in "First name", with: new_user.first_name
+      fill_in "Last name", with: new_user.last_name
+      fill_in "Username", with: new_user.username
       click_on "Sign up"
 
       expect(page).to have_content "Password is too short (minimum is 6 characters)"
+    end
+  end
+
+  context "with taken email" do
+    it "shows you an error message" do
+      visit new_user_registration_path
+
+      fill_in "Email", with: taken_user.email
+      fill_in "Password", with: new_user.password
+      fill_in "Password confirmation", with: new_user.password_confirmation
+      fill_in "First name", with: new_user.first_name
+      fill_in "Last name", with: new_user.last_name
+      fill_in "Username", with: new_user.username
+      click_on "Sign up"
+
+      expect(page).to have_content "Email has already been taken"
+    end
+  end
+
+  context "with taken username" do
+    it "shows you an error message" do
+      visit new_user_registration_path
+
+      fill_in "Email", with: new_user.email
+      fill_in "Password", with: new_user.password
+      fill_in "Password confirmation", with: new_user.password_confirmation
+      fill_in "First name", with: new_user.first_name
+      fill_in "Last name", with: new_user.last_name
+      fill_in "Username", with: taken_user.username
+      click_on "Sign up"
+
+      expect(page).to have_content "Username has already been taken"
     end
   end
 
@@ -33,9 +72,12 @@ RSpec.describe "User registers", type: :feature do
     it "shows you the member dashboard" do
       visit new_user_registration_path
 
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password
-      fill_in "Password confirmation", with: user.password
+      fill_in "Email", with: new_user.email
+      fill_in "Password", with: new_user.password
+      fill_in "Password confirmation", with: new_user.password
+      fill_in "First name", with: new_user.first_name
+      fill_in "Last name", with: new_user.last_name
+      fill_in "Username", with: new_user.username
       click_on "Sign up"
 
       expect(page).to have_content "Dashboard Page"
